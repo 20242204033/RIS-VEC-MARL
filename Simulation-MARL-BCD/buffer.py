32143afd75b23dc5e5079059c7e2f6df28a3ba59
20242204033 <2395853295@qq.com>
2025-09-21 12:46:55 +0800

    buffer.py

--- Simulation-MARL-BCD/buffer.py
+++ Simulation-MARL-BCD/buffer.py
@@ -4,14 +4,16 @@ class ReplayBuffer():
     def __init__(self, max_size, input_shape, n_actions, n_agents):
         self.mem_size = max_size
         self.mem_cntr = 0
-        self.state_memory = np.zeros((self.mem_size, input_shape * n_agents), dtype=np.float16)
-        self.action_memory = np.zeros((self.mem_size, n_actions * n_agents), dtype=np.float16)
-        self.reward_global_memory = np.zeros(self.mem_size)
-        self.reward_local_memory =  np.zeros((self.mem_size, n_agents), dtype=np.float16)
-        self.new_state_memory = np.zeros((self.mem_size, input_shape * n_agents), dtype=np.float16)
+        self.state_memory = np.zeros((self.mem_size, input_shape * n_agents), dtype=np.float32)
+        self.action_memory = np.zeros((self.mem_size, n_actions * n_agents), dtype=np.float32)
+        self.reward_global_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.reward_local_memory =  np.zeros((self.mem_size, n_agents), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, input_shape * n_agents), dtype=np.float32)
         self.terminal_memory = np.zeros(self.mem_size, dtype=bool)
+        # 新增：按行展平的可行性掩码
+        self.mask_memory = np.zeros((self.mem_size, n_agents * n_agents), dtype=np.float32)
 
-    def store_transition(self, state, action, reward_g, reward_l, state_, done):
+    def store_transition(self, state, action, reward_g, reward_l, state_, done, mask_flat):
         index = self.mem_cntr % self.mem_size
         self.state_memory[index] = state
         self.action_memory[index] = action
@@ -19,19 +21,18 @@ class ReplayBuffer():
         self.reward_local_memory[index] = reward_l
         self.new_state_memory[index] = state_
         self.terminal_memory[index] = done
-
+        self.mask_memory[index] = mask_flat
         self.mem_cntr += 1
 
     def sample_buffer(self, batch_size):
         max_mem = min(self.mem_cntr, self.mem_size)
-
         batch = np.random.choice(max_mem, batch_size)
-
         states = self.state_memory[batch]
         actions = self.action_memory[batch]
         rewards_g = self.reward_global_memory[batch]
         rewards_l = self.reward_local_memory[batch]
         states_ = self.new_state_memory[batch]
         dones = self.terminal_memory[batch]
+        masks = self.mask_memory[batch]
+        return states, actions, rewards_g, rewards_l, states_, dones, masks
 
-        return states, actions, rewards_g, rewards_l, states_, dones
